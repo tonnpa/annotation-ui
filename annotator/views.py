97b14d870dbff3f35f6dc305a119e0da6ac8e6ae
Phan Anh Nguyen <tonnpa@gmail.com>
2016-12-03 00:23:53 -0500

    feat(ui): added annotation interface
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- annotator/views.py
+++ annotator/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Drug, Annotation
 
 
 def index(request):
-    context = {}
+    context = {
+        'drug': Drug.objects.get(key=16),
+        'annotations': Annotation.objects.filter(key=16),
+        'options': Annotation.ANNOTATION,
+    }
     return render(request, 'index.html', context)
