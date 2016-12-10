8ae72db1de9f494f6318a8621e45b342ddbecf87
Phan Anh Nguyen <tonnpa@gmail.com>
2016-12-09 23:21:39 -0500

    feat(ui): adding new annotations is working
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- annotator/static/js/annotation.js
+++ annotator/static/js/annotation.js
@@ -1,6 +1,25 @@
 /**
  * Created by tonnpa on 12/9/16.
  */
+
+function addAnnotation(drugID, cui, mdr1, mark) {
+    console.log(JSON.stringify({
+            cui: cui,
+            mdr1: mdr1,
+            annotation: mark
+        }));
+    return $.ajax(prepRequest({
+        url: "/annotations/",
+        type: "POST",
+        data: JSON.stringify({
+            key: drugID,
+            cui: cui,
+            mdr1: mdr1,
+            annotation: mark
+        })
+    }));
+}
+
 function updateAnnotation(annotationID, mark) {
     return $.ajax(prepRequest({
         url: "/annotations/" + annotationID + "/",
