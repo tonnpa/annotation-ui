e7a8650638c62e139b1f782c0058e5e6c42498f2
aninguyen <phuong.a.nguyen2@gmail.com>
2017-02-02 17:35:26 +0000

    solved serializer error: AssertionError: Creating a ModelSerializer without either the 'fields' attribute or the 'exclude' attribute has been deprecated since 3.3.0, and is now disallowed. Add an explicit fields = '__all__' to the AnnotationSerializer serializer.

--- annotator/serializers.py
+++ annotator/serializers.py
@@ -5,8 +5,10 @@ from annotator.models import Drug, Annotation
 class DrugSerializer(ModelSerializer):
     class Meta:
         model = Drug
+        fields = '__all__'
 
 
 class AnnotationSerializer(ModelSerializer):
     class Meta:
         model = Annotation
+        fields = '__all__'
